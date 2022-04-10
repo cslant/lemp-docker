a031660fea1d07502ee1521f58519e3ed859314d
TanNguyen <phuongtan12357nguyen@gmail.com>
2022-04-10 16:43:33 +0700

    fix root path to run ssl

--- laravel-docker/docker/config/docker-entroypoint.sh
+++ laravel-docker/docker/config/docker-entroypoint.sh
@@ -29,6 +29,7 @@ if [[ "$UPDATE_UID_GID" = "true" ]]; then
 fi
 
 chown www-data:www-data /var/www
+chmod 777 /var/www
 
 [ "$PHP_ENABLE_XDEBUG" = "true" ] && \
     docker-php-ext-enable xdebug && \
